bd4bd58441267d3b69935a54c299d22398db6f50
minhquangd0103-ship-it <minhquangd0103@gmail.com>
2026-08-07 22:32:21 -0400

    Add files via upload

---
+++ Firmware/keymaps/default/keymap.c
@@ -0,0 +1,18 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+       KC_UP,   KC_SPACE,   KC_UP
+    )
+};
+#ifdef OLED_ENABLE
+
+ bool oled_task_user(void) (
+    oled_write_P(PSTR("Jump"), false);
+    return false;
+ )
+
+ #endif
